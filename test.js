512ea1cd2b4c213c6cd3d7e63112376ce78670dd
Ron Wilson <rwilson1@underarmour.com>
2016-11-15 13:50:03 -0500

    fix the test to support marked > 0.3.2; remove _readme.md

--- test.js
+++ test.js
@@ -1,14 +1,12 @@
-var md2conflu = require('./')
-var assert = require('assert')
+var md2conflu = require('./'),
+	assert = require('assert');
 
-var pairs = [
-      ['#h1', 'h1. h1\n\n']
-    , ['head1\n===', 'h1. head1\n\n']
-    , ['###  h3', 'h3. h3\n\n']
-]
-
-pairs.forEach(function(arr, i) {
-    assert.equal(md2conflu(arr[0]), arr[1], i + ': ' + arr[0] + ' = ' + arr[1])
+[
+	  ['# h1', 'h1. h1\n\n']
+	, ['head1\n===', 'h1. head1\n\n']
+	, ['### h3', 'h3. h3\n\n']
+].forEach(function(arr, i) {
+	assert.equal(md2conflu(arr[0]), arr[1], [i,': ',arr[0],' = ',arr[1]].join(''));
 })
 
 console.log('all pass!')
